5a6fc18aab2719e21ebdda1a7e09b5405dc2fa1b
Hung Phan Viet <hungphan@ahachat.com>
2026-03-06 18:16:48 +0700

    fix: messenger integration and inbox auth handling

--- integrations/messenger/src/integration.ts
+++ integrations/messenger/src/integration.ts
@@ -7,6 +7,7 @@ import { getUserProfile } from "./apis/user"
 import { agentMarkAsRead, sendTyping } from "./conversation"
 import { MessengerAPIException } from "./exception"
 import { webhookHandler } from "./handlers/webhook"
+import { receiveMessage } from "./incomming-message"
 import { sendFlowStep, sendMessage } from "./outgoing-message"
 import type {
   MessengerActions,
@@ -23,6 +24,7 @@ const config: IntegrationDefinition<
   channels: {
     channel: {
       message: {
+        receiveMessage,
         sendMessage,
       },
       conversation: {
