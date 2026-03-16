187ffee662325a0102fb1390dfd35481a8c060db
Sung <54098841+sunghajung43@users.noreply.github.com>
2026-03-16 23:54:20 +0700

    Update whatsapp UI (#298)

--- apps/builder/src/components/app-tab.tsx
+++ apps/builder/src/components/app-tab.tsx
@@ -2,31 +2,28 @@
 
 import { Card, CardContent } from "@aha.chat/ui/components/ui/card"
 import Link from "next/link"
-import { useTranslations } from "next-intl"
 
-export function AppTab({ chatbotId }: { chatbotId: string }) {
-  const t = useTranslations()
+type AppTabProps = {
+  tabs: {
+    label: string
+    href: string
+    isActive: boolean
+  }[]
+}
+
+export function AppTab({ tabs }: AppTabProps) {
   return (
-    <Card>
+    <Card className="py-0">
       <CardContent className="flex items-center gap-8 px-8">
-        <Link
-          className="font-medium text-sm"
-          href={`/chatbots/${chatbotId}/tags`}
-        >
-          {t("tags.title")}
-        </Link>
-        <Link
-          className="font-medium text-sm"
-          href={`/chatbots/${chatbotId}/custom-fields`}
-        >
-          {t("customFields.title")}
-        </Link>
-        <Link
-          className="font-medium text-sm"
-          href={`/chatbots/${chatbotId}/error-logs`}
-        >
-          {t("errorLogs.title")}
-        </Link>
+        {tabs.map((tab) => (
+          <Link
+            className={`border-b-2 py-6 text-sm ${tab.isActive ? "border-neutral-700 dark:border-white dark:text-gray-50" : "border-transparent font-medium text-gray-800 dark:text-gray-400"}`}
+            href={tab.href}
+            key={tab.href}
+          >
+            {tab.label}
+          </Link>
+        ))}
       </CardContent>
     </Card>
   )
