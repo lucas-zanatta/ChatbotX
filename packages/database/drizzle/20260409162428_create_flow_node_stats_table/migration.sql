edf67d6a13c1f76672650154f0f2bf4865f6b476
Hung Phan Viet <hungphan@ahachat.com>
2026-04-10 00:06:06 +0700

    feat: flow analytics

---
+++ packages/database/drizzle/20260409162428_create_flow_node_stats_table/migration.sql
@@ -0,0 +1,34 @@
+CREATE TABLE "FlowAnalyticsSession" (
+	"id" bigint PRIMARY KEY,
+	"createdAt" timestamp(6) with time zone DEFAULT now() NOT NULL,
+	"updatedAt" timestamp(6) with time zone DEFAULT now() NOT NULL,
+	"flowId" text NOT NULL,
+	"workspaceId" text NOT NULL,
+	"deletedAt" timestamp(6) with time zone
+);
+--> statement-breakpoint
+CREATE TABLE "FlowNodeStat" (
+	"id" bigint PRIMARY KEY,
+	"createdAt" timestamp(6) with time zone DEFAULT now() NOT NULL,
+	"updatedAt" timestamp(6) with time zone DEFAULT now() NOT NULL,
+	"workspaceId" bigint NOT NULL,
+	"flowId" bigint NOT NULL,
+	"analyticsId" bigint NOT NULL,
+	"nodeId" text NOT NULL,
+	"buttonId" text,
+	"contactId" bigint NOT NULL,
+	"contactInboxId" bigint NOT NULL,
+	"failedAt" timestamp(6) with time zone,
+	"deliveredAt" timestamp(6) with time zone,
+	"seenAt" timestamp(6) with time zone,
+	"clickedAt" timestamp(6) with time zone
+);
+--> statement-breakpoint
+CREATE UNIQUE INDEX "FlowAnalyticsSession_workspaceId_chatbotId_key" ON "FlowAnalyticsSession" ("flowId" text_ops,"workspaceId" text_ops) WHERE "deletedAt" IS NULL;--> statement-breakpoint
+CREATE INDEX "FlowAnalyticsSession_flowId_idx" ON "FlowAnalyticsSession" ("flowId");--> statement-breakpoint
+CREATE INDEX "FlowNodeStat_contactInboxId_seenAt_idx" ON "FlowNodeStat" ("contactInboxId","seenAt");--> statement-breakpoint
+CREATE INDEX "FlowNodeStat_analyticsId_nodeId_idx" ON "FlowNodeStat" ("analyticsId","nodeId");--> statement-breakpoint
+CREATE UNIQUE INDEX "FlowNodeStat_analyticsId_nodeId_contactId_key" ON "FlowNodeStat" ("analyticsId","nodeId","contactId");--> statement-breakpoint
+ALTER TABLE "FlowAnalyticsSession" ADD CONSTRAINT "FlowAnalyticsSession_flowId_fkey" FOREIGN KEY ("flowId") REFERENCES "Flow"("id") ON DELETE CASCADE ON UPDATE CASCADE;--> statement-breakpoint
+ALTER TABLE "FlowAnalyticsSession" ADD CONSTRAINT "FlowAnalyticsSession_workspaceId_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;--> statement-breakpoint
+ALTER TABLE "FlowNodeStat" ADD CONSTRAINT "FlowNodeStat_workspaceId_Workspace_id_fkey" FOREIGN KEY ("workspaceId") REFERENCES "Workspace"("id") ON DELETE CASCADE ON UPDATE CASCADE;
