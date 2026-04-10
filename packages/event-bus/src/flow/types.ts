f0fa64ee4c11ec459d9fb01249211518cfbd948b
Hung Phan Viet <hungphan@ahachat.com>
2026-04-10 16:12:04 +0700

    feat: flow analytics

--- packages/event-bus/src/flow/types.ts
+++ packages/event-bus/src/flow/types.ts
@@ -1,16 +1,12 @@
-import type { BaseEventListener, BaseEventType, InferEventMap } from "../types"
 import type { z } from "zod"
-import type {
-  clickedPayloadSchema,
-  flowEventSchemas,
-} from "./schemas"
+import type { BaseEventListener, BaseEventType, InferEventMap } from "../types"
+import type { clickedPayloadSchema, flowEventSchemas } from "./schemas"
 
 export const FlowEventType: BaseEventType = {
   CLICKED: "flow:clicked",
 } as const
 
-export type FlowEventType =
-  (typeof FlowEventType)[keyof typeof FlowEventType]
+export type FlowEventType = (typeof FlowEventType)[keyof typeof FlowEventType]
 
 export type FlowEventMap = InferEventMap<typeof flowEventSchemas>
 export type FlowPayload = FlowEventMap[FlowEventType]
@@ -19,7 +15,4 @@ export type FlowClickedPayload = z.infer<typeof clickedPayloadSchema>
 
 export interface FlowEventListener extends BaseEventListener<FlowPayload> {}
 
-export type FlowEvenTypeMap = Record<
-  FlowEventType,
-  FlowEventListener[]
->
+export type FlowEvenTypeMap = Record<FlowEventType, FlowEventListener[]>
