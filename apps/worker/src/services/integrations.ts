646f8adfdf0bbfedc6cb1da6c7e750c0bf3f8da4
Real Codesiman <realcodesiman@gmail.com>
2026-04-23 16:10:28 +0700

    fix: add instagram to worker

--- apps/worker/src/services/integrations.ts
+++ apps/worker/src/services/integrations.ts
@@ -8,6 +8,7 @@ import type {
 } from "@chatbotx.io/database/types"
 import { integration as integrationChatbotx } from "@chatbotx.io/integration-chatbotx"
 import { integration as integrationGoogleSheets } from "@chatbotx.io/integration-google-sheets"
+import { integration as integrationInstagram } from "@chatbotx.io/integration-instagram"
 import { integration as integrationMessenger } from "@chatbotx.io/integration-messenger"
 import { integration as integrationSmtp } from "@chatbotx.io/integration-smtp"
 import { integration as integrationTelegram } from "@chatbotx.io/integration-telegram"
@@ -36,6 +37,7 @@ export const allIntegrations: Record<
   zalo: integrationZalo,
   chatbotx: integrationChatbotx,
   smtp: integrationSmtp,
+  instagram: integrationInstagram,
 }
 
 export const integrationService = {
